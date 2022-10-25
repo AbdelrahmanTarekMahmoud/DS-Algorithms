1210bbc34ff93c8c17ea7741db76989b4dbae8d1
Abdelrahman Tarek Mahmoud <85140058+AbdelrahmanTarekMahmoud@users.noreply.github.com>
2022-10-25 19:28:42 +0200

    Create 10-TopologicalSortingToDetectAcycle.cpp

---
+++ DS implementation/Graphs/10-TopologicalSortingToDetectAcycle.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include <utility>
+#include <stack>
+
+
+class Solution
+{
+public:
+     
+    bool IsCyclic(int N, std::vector<int> adj[])
+    {
+        std::queue<int> Queue;
+        //first we will calculate indegree of every Node
+        /*
+        * Example :
+        * 0 -> -
+        * 1 -> -
+        * 2 -> 3
+        * 3 -> 1
+        * 4 -> 0 , 1
+        * 5 -> 0 , 2
+        */
+        std::vector<int> inDegree(N + 1, 0);
+        for (int i = 0; i < N;i++)
+        {
+            for (auto it : adj[i])
+            {
+                inDegree[it] ++;
+            }
+        }
+        //Then we will push Node with zero degree into out Queue
+        for (int i = 0; i < N;i++)
+        {
+            if (inDegree[i] == 0)
+            {
+                Queue.push(i);
+            }
+        }
+        //Third we will process our nodes (-1 for indegree every time we visit it node to remove dependency )
+        std::vector<int> topoResult;
+
+        int Counter = 0;  // Counter used to compare its value with all Nodes  
+
+        while (!Queue.empty())
+        {
+            int node = Queue.front();
+            Queue.pop();
+            Counter++;
+            for (auto it : adj[node])
+            {
+                inDegree[it]--;
+                if (inDegree[it] == 0)
+                {
+                    Queue.push(it);
+                }
+            }
+        }
+        if (Counter == N) return false;
+        return true;
+    }
+};
+
